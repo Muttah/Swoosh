0497d2b3eadcb3da67357ec973dd208f47058802
Stanislav <kenny2lighter@gmail.com>
2018-09-09 20:21:40 +0300

    lesson 1. Welcome layout

--- app/src/main/java/com/example/stanislavp/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/stanislavp/swoosh/WelcomeActivity.kt
@@ -2,11 +2,17 @@ package com.example.stanislavp.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            Toast.makeText(this@WelcomeActivity, "Tu pidor", Toast.LENGTH_LONG).show()
+        }
     }
 }
